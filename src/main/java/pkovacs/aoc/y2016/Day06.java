1589b3b61c23f1491c7ca28319418c850f702299
Péter Kovács <kovacspeter84@gmail.com>
2021-08-29 01:26:20 +0200

    Add solution for Day 6

--- src/main/java/pkovacs/aoc/y2016/Day06.java
+++ src/main/java/pkovacs/aoc/y2016/Day06.java
@@ -1,5 +1,9 @@
 package pkovacs.aoc.y2016;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import pkovacs.aoc.util.InputUtils;
 
 public class Day06 {
@@ -7,8 +11,27 @@ public class Day06 {
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day06.txt");
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        int length = lines.get(0).length();
+
+        var msg1 = new StringBuilder();
+        var msg2 = new StringBuilder();
+        for (int k = 0; k < length; k++) {
+            var counts = new HashMap<Character, Integer>();
+            for (var line : lines) {
+                counts.merge(line.charAt(k), 1, Integer::sum);
+            }
+            char mostCommon = counts.entrySet().stream()
+                    .max(Comparator.comparing(Entry::getValue))
+                    .get().getKey();
+            char leastCommon = counts.entrySet().stream()
+                    .min(Comparator.comparing(Entry::getValue))
+                    .get().getKey();
+            msg1.append(mostCommon);
+            msg2.append(leastCommon);
+        }
+
+        System.out.println("Part 1: " + msg1);
+        System.out.println("Part 2: " + msg2);
     }
 
 }
--- src/test/java/pkovacs/aoc/y2016/Day06Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day06Test.java
@@ -7,8 +7,8 @@ class Day06Test extends DayTestBase {
     @Test
     public void test() {
         Day06.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("qoclwvah");
+        assertSolution2("ryrgviuv");
     }
 
 }
