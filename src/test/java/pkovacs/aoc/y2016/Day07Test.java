c5108f3a8fe1c056f353c33b34ed86466408b96d
Péter Kovács <kovacspeter84@gmail.com>
2021-08-29 01:43:57 +0200

    Add solution for Day 7

--- src/test/java/pkovacs/aoc/y2016/Day07Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day07Test.java
@@ -7,8 +7,8 @@ class Day07Test extends DayTestBase {
     @Test
     public void test() {
         Day07.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("118");
+        assertSolution2("260");
     }
 
 }
