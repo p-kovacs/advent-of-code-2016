c2ffd366917d5edd2f12c04043f1729f1ef88986
Péter Kovács <kovacspeter84@gmail.com>
2021-08-29 00:20:47 +0200

    Add solution for Day 3

--- src/main/java/pkovacs/aoc/y2016/Day03.java
+++ src/main/java/pkovacs/aoc/y2016/Day03.java
@@ -5,10 +5,34 @@ import pkovacs.aoc.util.InputUtils;
 public class Day03 {
 
     public static void main(String[] args) {
-        var lines = InputUtils.readLines("y2016/day03.txt");
+        var ints = InputUtils.readInts("y2016/day03.txt");
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        int cnt1 = 0;
+        for (int i = 0; i < ints.length; i += 3) {
+            int a = ints[i];
+            int b = ints[i + 1];
+            int c = ints[i + 2];
+            if (a + b > c && a + c > b && b + c > a) {
+                cnt1++;
+            }
+        }
+
+        int cnt2 = 0;
+        for (int i = 0; i < ints.length; ) {
+            int a = ints[i];
+            int b = ints[i + 3];
+            int c = ints[i + 6];
+            if (a + b > c && a + c > b && b + c > a) {
+                cnt2++;
+            }
+            i++;
+            if (i % 3 == 0) {
+                i += 6;
+            }
+        }
+
+        System.out.println("Part 1: " + cnt1);
+        System.out.println("Part 2: " + cnt2);
     }
 
 }
--- src/test/java/pkovacs/aoc/y2016/Day03Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day03Test.java
@@ -7,8 +7,8 @@ class Day03Test extends DayTestBase {
     @Test
     public void test() {
         Day03.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("869");
+        assertSolution2("1544");
     }
 
 }
