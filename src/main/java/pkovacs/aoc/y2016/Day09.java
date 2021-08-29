7cd9e549ccf94bca32b81e8404e1a0356d934bcc
Péter Kovács <kovacspeter84@gmail.com>
2021-08-29 02:39:54 +0200

    Add solution for Day 9

--- src/main/java/pkovacs/aoc/y2016/Day09.java
+++ src/main/java/pkovacs/aoc/y2016/Day09.java
@@ -6,9 +6,35 @@ public class Day09 {
 
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day09.txt");
+        var input = lines.get(0);
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + getDecompressedLength(input, false));
+        System.out.println("Part 2: " + getDecompressedLength(input, true));
+    }
+
+    private static long getDecompressedLength(String str, boolean recursive) {
+        long total = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') {
+                continue;
+            }
+            if (ch == '(') {
+                var endIndex = str.indexOf(')', i);
+                var marker = str.substring(i + 1, endIndex);
+
+                int charCount = Integer.parseInt(marker.split("x")[0]);
+                int repetitionCount = Integer.parseInt(marker.split("x")[1]);
+                var toRepeat = str.substring(endIndex + 1, endIndex + charCount + 1);
+                var length = recursive ? getDecompressedLength(toRepeat, true) : toRepeat.length();
+                total += length * repetitionCount;
+
+                i = endIndex + charCount;
+            } else {
+                total++;
+            }
+        }
+        return total;
     }
 
 }
--- src/test/java/pkovacs/aoc/y2016/Day09Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day09Test.java
@@ -7,8 +7,8 @@ class Day09Test extends DayTestBase {
     @Test
     public void test() {
         Day09.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("110346");
+        assertSolution2("10774309173");
     }
 
 }
