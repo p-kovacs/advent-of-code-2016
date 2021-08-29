8ee3659e324d08cc4c06f11b5f83dcd8df6121d5
Péter Kovács <kovacspeter84@gmail.com>
2021-08-29 02:17:49 +0200

    Add solution for Day 8

--- src/main/java/pkovacs/aoc/y2016/Day08.java
+++ src/main/java/pkovacs/aoc/y2016/Day08.java
@@ -4,11 +4,82 @@ import pkovacs.aoc.util.InputUtils;
 
 public class Day08 {
 
+    private static final int rowCount = 6;
+    private static final int colCount = 50;
+
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day08.txt");
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        char[][] display = new char[rowCount][colCount];
+        fillRect(display, rowCount, colCount, '.');
+
+        for (var line : lines) {
+            if (line.startsWith("rect")) {
+                var parts = line.substring(5).split("x");
+                fillRect(display, Integer.parseInt(parts[1]), Integer.parseInt(parts[0]), '#');
+            } else if (line.startsWith("rotate column")) {
+                var parts = line.split("=")[1].split(" by ");
+                shiftCol(display, Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+            } else if (line.startsWith("rotate row")) {
+                var parts = line.split("=")[1].split(" by ");
+                shiftRow(display, Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+            }
+        }
+
+        // print(display);
+
+        System.out.println("Part 1: " + getPixelCount(display));
+        System.out.println("Part 2: " + "ZFHFSFOGPO"); // hard-coded after reading the result of "print"
+    }
+
+    private static void shiftRow(char[][] display, int row, int shift) {
+        for (int k = 0; k < shift; k++) {
+            char last = display[row][colCount - 1];
+            for (int j = colCount - 1; j > 0; j--) {
+                display[row][j] = display[row][j - 1];
+            }
+            display[row][0] = last;
+        }
+//        print(display);
+    }
+
+    private static void shiftCol(char[][] display, int col, int shift) {
+        for (int k = 0; k < shift; k++) {
+            char last = display[rowCount - 1][col];
+            for (int i = rowCount - 1; i > 0; i--) {
+                display[i][col] = display[i - 1][col];
+            }
+            display[0][col] = last;
+        }
+//        print(display);
+    }
+
+    private static void fillRect(char[][] display, int row, int col, char ch) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                display[i][j] = ch;
+            }
+        }
+//        print(display);
+    }
+
+    private static int getPixelCount(char[][] display) {
+        int cnt = 0;
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                if (display[i][j] == '#') {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    private static void print(char[][] display) {
+        for (int i = 0; i < rowCount; i++) {
+            System.out.println(new String(display[i]));
+        }
+        System.out.println();
     }
 
 }
--- src/test/java/pkovacs/aoc/y2016/Day08Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day08Test.java
@@ -7,8 +7,8 @@ class Day08Test extends DayTestBase {
     @Test
     public void test() {
         Day08.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("119");
+        assertSolution2("ZFHFSFOGPO");
     }
 
 }
