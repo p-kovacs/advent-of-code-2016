08f263e27e47b059a52679a82fefc4997c733039
Péter Kovács <kovacspeter84@gmail.com>
2021-08-29 03:06:55 +0200

    Add solution for Day 10

--- src/main/java/pkovacs/aoc/y2016/Day10.java
+++ src/main/java/pkovacs/aoc/y2016/Day10.java
@@ -1,5 +1,11 @@
 package pkovacs.aoc.y2016;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
 import pkovacs.aoc.util.InputUtils;
 
 public class Day10 {
@@ -7,8 +13,52 @@ public class Day10 {
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day10.txt");
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        ListMultimap<Integer, Long> bots = MultimapBuilder.hashKeys().arrayListValues().build();
+        Map<Integer, Long> outputs = new HashMap<>();
+
+        for (var line : lines) {
+            if (line.startsWith("value")) {
+                var parts = InputUtils.scan(line, "value %d goes to bot %d");
+                long value = parts.get(0).asLong();
+                int bot = parts.get(1).asInt();
+                bots.put(bot, value);
+            }
+        }
+
+        int result1 = -1;
+        while (!bots.isEmpty()) {
+            for (var line : lines) {
+                if (line.startsWith("bot")) {
+                    var parts = InputUtils.scan(line, "bot %d gives low to %s and high to %s");
+                    int bot = parts.get(0).asInt();
+                    if (bots.get(bot).size() == 2) {
+                        long low = Math.min(bots.get(bot).get(0), bots.get(bot).get(1));
+                        long high = Math.max(bots.get(bot).get(0), bots.get(bot).get(1));
+                        put(bots, outputs, low, parts.get(1).asString());
+                        put(bots, outputs, high, parts.get(2).asString());
+                        bots.removeAll(bot);
+
+                        if (low == 17 && high == 61) {
+                            result1 = bot;
+                        }
+                    }
+                }
+            }
+        }
+
+        long result2 = outputs.get(0) * outputs.get(1) * outputs.get(2);
+
+        System.out.println("Part 1: " + result1);
+        System.out.println("Part 2: " + result2);
+    }
+
+    private static void put(Multimap<Integer, Long> bots, Map<Integer, Long> outputs, long value, String destination) {
+        int id = Integer.parseInt(destination.split(" ")[1]);
+        if (destination.startsWith("output")) {
+            outputs.put(id, value);
+        } else {
+            bots.put(id, value);
+        }
     }
 
 }
--- src/test/java/pkovacs/aoc/y2016/Day10Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day10Test.java
@@ -7,8 +7,8 @@ class Day10Test extends DayTestBase {
     @Test
     public void test() {
         Day10.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("141");
+        assertSolution2("1209");
     }
 
 }
