05f5e123d76c0de4287733f0f525c0cc51fe50e8
Péter Kovács <kovacspeter84@gmail.com>
2021-09-18 13:53:25 +0200

    Add solution for Day 16

--- src/test/java/pkovacs/aoc/y2016/Day16Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day16Test.java
@@ -7,8 +7,8 @@ class Day16Test extends DayTestBase {
     @Test
     public void test() {
         Day16.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("10111110010110110");
+        assertSolution2("01101100001100100");
     }
 
 }
