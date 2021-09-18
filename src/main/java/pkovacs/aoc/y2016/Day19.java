d0bdcb2522f3c15e3f8c8375a2cc2b0bec5d8f61
Péter Kovács <kovacspeter84@gmail.com>
2021-09-18 17:30:31 +0200

    Add solution for Day 19

--- src/main/java/pkovacs/aoc/y2016/Day19.java
+++ src/main/java/pkovacs/aoc/y2016/Day19.java
@@ -5,10 +5,32 @@ import pkovacs.aoc.util.InputUtils;
 public class Day19 {
 
     public static void main(String[] args) {
-        var lines = InputUtils.readLines("y2016/day19.txt");
+        int playerCount = Integer.parseInt(InputUtils.readLine("y2016/day19.txt"));
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        System.out.println("Part 1: " + play(playerCount, false));
+        System.out.println("Part 2: " + play(playerCount, true));
+    }
+
+    public static int play(int playerCount, boolean advanced) {
+        // Build a double-linked list of the player IDs
+        int[] next = new int[playerCount];
+        int[] prev = new int[playerCount];
+        for (int i = 0; i < next.length - 1; i++) {
+            next[i] = i + 1;
+            prev[i + 1] = i;
+        }
+        next[next.length - 1] = 0;
+        prev[0] = next.length - 1;
+
+        // Play until only one player is left
+        int i = advanced ? playerCount / 2 : 1; // denotes the next player to steal from
+        for (int playingCount = playerCount; playingCount > 1; playingCount--) {
+            next[prev[i]] = next[i];
+            prev[next[i]] = prev[i];
+            i = advanced && (playingCount % 2 == 0) ? next[i] : next[next[i]];
+        }
+
+        return i + 1;
     }
 
 }
--- src/test/java/pkovacs/aoc/y2016/Day19Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day19Test.java
@@ -7,8 +7,8 @@ class Day19Test extends DayTestBase {
     @Test
     public void test() {
         Day19.main(null);
-        assertSolution1("0");
-        assertSolution2("0");
+        assertSolution1("1842613");
+        assertSolution2("1424135");
     }
 
 }
