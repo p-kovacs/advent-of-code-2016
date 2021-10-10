02dec4a19285d3857d3eda903ba9fab5e75b155b
Péter Kovács <kovacspeter84@gmail.com>
2021-10-10 18:06:02 +0200

    Refactoring in Day12

--- src/main/java/pkovacs/aoc/y2016/Day12.java
+++ src/main/java/pkovacs/aoc/y2016/Day12.java
@@ -10,37 +10,37 @@ public class Day12 {
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day12.txt");
 
-        var registers = new CounterMap<String>();
+        var mem1 = new CounterMap<String>();
 
-        var registers2 = new CounterMap<String>();
-        registers2.put("c", 1L);
+        var mem2 = new CounterMap<String>();
+        mem2.put("c", 1L);
 
-        System.out.println("Part 1: " + solve(lines, registers));
-        System.out.println("Part 2: " + solve(lines, registers2));
+        System.out.println("Part 1: " + solve(lines, mem1));
+        System.out.println("Part 2: " + solve(lines, mem2));
     }
 
-    private static long solve(List<String> code, CounterMap<String> registers) {
+    private static long solve(List<String> code, CounterMap<String> mem) {
         for (int i = 0; i < code.size(); ) {
             var p = code.get(i).split(" ");
             switch (p[0]) {
-                case "cpy" -> registers.put(p[2], getValue(registers, p[1]));
-                case "inc" -> registers.inc(p[1]);
-                case "dec" -> registers.dec(p[1]);
+                case "cpy" -> mem.put(p[2], getValue(mem, p[1]));
+                case "inc" -> mem.inc(p[1]);
+                case "dec" -> mem.dec(p[1]);
                 case "jnz" -> {
-                    long value = getValue(registers, p[1]);
+                    long value = getValue(mem, p[1]);
                     if (value != 0) {
-                        i += Integer.parseInt(p[2]);
+                        i += (int) getValue(mem, p[2]);
                         continue; // skip standard step
                     }
                 }
             }
             i++;
         }
-        return registers.get("a");
+        return mem.get("a");
     }
 
-    private static long getValue(CounterMap<String> registers, String arg) {
-        return Character.isDigit(arg.charAt(0)) ? Long.parseLong(arg) : registers.get(arg);
+    private static long getValue(CounterMap<String> mem, String arg) {
+        return Character.isLetter(arg.charAt(0)) ? mem.get(arg) : Long.parseLong(arg);
     }
 
 }
