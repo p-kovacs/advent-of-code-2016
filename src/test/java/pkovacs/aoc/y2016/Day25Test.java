6f473f65faf8f68fabd32b67f7f8fe329ba3ef59
Péter Kovács <kovacspeter84@gmail.com>
2021-10-13 09:04:41 +0200

    Add solution for Day 25

--- src/test/java/pkovacs/aoc/y2016/Day25Test.java
+++ src/test/java/pkovacs/aoc/y2016/Day25Test.java
@@ -7,7 +7,7 @@ class Day25Test extends DayTestBase {
     @Test
     public void test() {
         Day25.main(null);
-        assertSolution1("0");
+        assertSolution1("196");
         assertSolution2("0");
     }
 
