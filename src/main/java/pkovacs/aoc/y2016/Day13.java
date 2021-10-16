a07e85d4d46a2d6a64ee33a84ab781efb9ab17dd
Péter Kovács <kovacspeter84@gmail.com>
2021-10-16 08:36:56 +0200

    Add Point util class

--- src/main/java/pkovacs/aoc/y2016/Day13.java
+++ src/main/java/pkovacs/aoc/y2016/Day13.java
@@ -2,7 +2,7 @@ package pkovacs.aoc.y2016;
 
 import pkovacs.aoc.util.Bfs;
 import pkovacs.aoc.util.InputUtils;
-import pkovacs.aoc.util.Tile;
+import pkovacs.aoc.util.Point;
 
 public class Day13 {
 
@@ -11,18 +11,18 @@ public class Day13 {
     public static void main(String[] args) {
         long shift = InputUtils.readLongs("y2016/day13.txt")[0];
 
-        var start = new Tile(1, 1);
-        var target = new Tile(39, 31);
+        var start = new Point(1, 1);
+        var target = new Point(31, 39);
 
         var results = Bfs.run(start,
-                tile -> tile.validNeighbors(MAP_SIZE, MAP_SIZE, n -> isNotWall(n, shift)));
+                point -> point.validNeighbors(MAP_SIZE, MAP_SIZE, p -> isNotWall(p, shift)));
 
         System.out.println("Part 1: " + results.get(target).getDist());
         System.out.println("Part 2: " + results.values().stream().filter(res -> res.getDist() <= 50).count());
     }
 
-    private static boolean isNotWall(Tile tile, long shift) {
-        return (Long.bitCount(getMagicNumber(tile.col(), tile.row(), shift)) & 1) == 0;
+    private static boolean isNotWall(Point point, long shift) {
+        return (Long.bitCount(getMagicNumber(point.x(), point.y(), shift)) & 1) == 0;
     }
 
     private static long getMagicNumber(long x, long y, long shift) {
--- src/main/java/pkovacs/aoc/y2016/Day22.java
+++ src/main/java/pkovacs/aoc/y2016/Day22.java
@@ -5,32 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import pkovacs.aoc.util.Bfs;
 import pkovacs.aoc.util.InputUtils;
-import pkovacs.aoc.util.Tile;
+import pkovacs.aoc.util.Point;
 
 public class Day22 {
 
     private static record Storage(int used, int avail) {}
 
-    private static record State(Tile data, Tile hole) {}
+    private static record State(Point data, Point hole) {}
 
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day22.txt");
 
-        Map<Tile, Storage> map = new HashMap<Tile, Storage>();
+        Map<Point, Storage> map = new HashMap<Point, Storage>();
         for (int i = 2; i < lines.size(); i++) {
             var line = lines.get(i);
             var parts = InputUtils.scan(line, "/dev/grid/node-x%d-y%d *%dT *%dT *%dT.*");
-            var tile = new Tile(parts.get(1).asInt(), parts.get(0).asInt());
+            var point = new Point(parts.get(0).asInt(), parts.get(1).asInt());
             var storage = new Storage(parts.get(3).asInt(), parts.get(4).asInt());
-            map.put(tile, storage);
+            map.put(point, storage);
         }
 
-        int rowCount = map.keySet().stream().mapToInt(Tile::row).max().getAsInt() + 1;
-        int colCount = map.keySet().stream().mapToInt(Tile::col).max().getAsInt() + 1;
+        long width = map.keySet().stream().mapToLong(Point::x).max().getAsLong() + 1;
+        long height = map.keySet().stream().mapToLong(Point::y).max().getAsLong() + 1;
 
         // For part 2, based on the analysis of the data, we can assume that there is only one "hole" (empty storage
         // node), and each feasible step moves data from and adjacent node to the hole. That is, each step moves the
@@ -42,13 +43,13 @@ public class Day22 {
 
         // Collect walls
         int minCapacity = map.values().stream().mapToInt(st -> st.used() + st.avail()).min().getAsInt();
-        Set<Tile> walls = map.entrySet().stream()
+        Set<Point> walls = map.entrySet().stream()
                 .filter(entry -> entry.getValue().used() > minCapacity)
                 .map(Entry::getKey)
                 .collect(Collectors.toSet());
 
         // Find initial positions of the data and the hole tiles
-        var dataStart = new Tile(0, colCount - 1);
+        var dataStart = new Point(width - 1, 0);
         var holeStart = map.entrySet().stream()
                 .filter(entry -> entry.getValue().used() == 0)
                 .map(Entry::getKey)
@@ -59,10 +60,7 @@ public class Day22 {
                 state -> {
                     boolean alreadyAdjacent = areAdjacent(state.data(), state.hole());
                     var nextStates = new ArrayList<State>();
-                    for (var newHole : state.hole().neighbors()) {
-                        if (!newHole.isValid(rowCount, colCount) || walls.contains(newHole)) {
-                            continue;
-                        }
+                    for (var newHole : state.hole().validNeighbors(width, height, Predicate.not(walls::contains))) {
                         var newData = newHole.equals(state.data()) ? state.hole() : state.data();
 
                         // Heuristic to prune search space: once the hole tile becomes adjacent to the data tile
@@ -76,22 +74,22 @@ public class Day22 {
                     }
                     return nextStates;
                 },
-                state -> new Tile(0, 0).equals(state.data())
+                state -> new Point(0, 0).equals(state.data())
         );
 
         System.out.println("Part 1: " + countViablePairs(map));
         System.out.println("Part 2: " + result.get().getDist());
     }
 
-    private static boolean areAdjacent(Tile t1, Tile t2) {
-        return Math.abs(t1.row() - t2.row()) <= 1 && Math.abs(t1.col() - t2.col()) <= 1;
+    private static boolean areAdjacent(Point p1, Point p2) {
+        return Math.abs(p1.x() - p2.x()) <= 1 && Math.abs(p1.y() - p2.y()) <= 1;
     }
 
-    private static int countViablePairs(Map<Tile, Storage> map) {
+    private static int countViablePairs(Map<Point, Storage> map) {
         int cnt = 0;
-        for (Tile t1 : map.keySet()) {
-            for (Tile t2 : map.keySet()) {
-                if (isViablePair(map, t1, t2)) {
+        for (Point p1 : map.keySet()) {
+            for (Point p2 : map.keySet()) {
+                if (isViablePair(map, p1, p2)) {
                     cnt++;
                 }
             }
@@ -99,8 +97,8 @@ public class Day22 {
         return cnt;
     }
 
-    private static boolean isViablePair(Map<Tile, Storage> map, Tile t1, Tile t2) {
-        return !t1.equals(t2) && map.get(t1).used() > 0 && map.get(t1).used() <= map.get(t2).avail();
+    private static boolean isViablePair(Map<Point, Storage> map, Point p1, Point p2) {
+        return !p1.equals(p2) && map.get(p1).used() > 0 && map.get(p1).used() <= map.get(p2).avail();
     }
 
 }
