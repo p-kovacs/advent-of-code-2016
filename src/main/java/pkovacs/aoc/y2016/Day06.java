fa8afdd8223ef7ee93b190f8c899415bd6326905
Péter Kovács <kovacspeter84@gmail.com>
2021-10-24 22:40:48 +0200

    Add alternative solution for Day06

--- src/main/java/pkovacs/aoc/y2016/Day06.java
+++ src/main/java/pkovacs/aoc/y2016/Day06.java
@@ -1,37 +1,56 @@
 package pkovacs.aoc.y2016;
 
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 import pkovacs.aoc.util.InputUtils;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
 
 public class Day06 {
 
     public static void main(String[] args) {
         var lines = InputUtils.readLines("y2016/day06.txt");
 
-        int length = lines.get(0).length();
+        System.out.println("Part 1: " + decode2(lines, false));
+        System.out.println("Part 2: " + decode2(lines, true));
+    }
 
-        var msg1 = new StringBuilder();
-        var msg2 = new StringBuilder();
-        for (int k = 0; k < length; k++) {
+    private static String decode1(List<String> lines, boolean reversed) {
+        var msg = new StringBuilder();
+        for (int k = 0; k < lines.get(0).length(); k++) {
             var counts = new HashMap<Character, Integer>();
             for (var line : lines) {
                 counts.merge(line.charAt(k), 1, Integer::sum);
             }
-            char mostCommon = counts.entrySet().stream()
-                    .max(Comparator.comparing(Entry::getValue))
-                    .get().getKey();
-            char leastCommon = counts.entrySet().stream()
-                    .min(Comparator.comparing(Entry::getValue))
-                    .get().getKey();
-            msg1.append(mostCommon);
-            msg2.append(leastCommon);
-        }
 
-        System.out.println("Part 1: " + msg1);
-        System.out.println("Part 2: " + msg2);
+            char ch = counts.entrySet().stream()
+                    .max(comparing(e -> reversed ? -e.getValue() : e.getValue()))
+                    .orElseThrow()
+                    .getKey();
+            msg.append(ch);
+        }
+        return msg.toString();
+    }
+
+    private static String decode2(List<String> lines, boolean reversed) {
+        return IntStream.range(0, lines.get(0).length())
+                .mapToObj(k -> lines.stream().collect(groupingBy(line -> line.charAt(k), counting())))
+                .map(Map::entrySet)
+                .map(Set::stream)
+                .map(s -> reversed ? s.min(Entry.comparingByValue()) : s.max(Entry.comparingByValue()))
+                .map(Optional::orElseThrow)
+                .map(Entry::getKey)
+                .map(String::valueOf)
+                .collect(joining());
     }
 
 }
