ebe991e3c73ca53a8f0ab99402da0834e79a17af
Péter Kovács <kovacspeter84@gmail.com>
2021-10-24 22:40:00 +0200

    Refactoring: use streams instead of loops

--- src/main/java/pkovacs/aoc/y2016/Day04.java
+++ src/main/java/pkovacs/aoc/y2016/Day04.java
@@ -1,11 +1,12 @@
 package pkovacs.aoc.y2016;
 
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import pkovacs.aoc.util.InputUtils;
+
+import static java.util.stream.Collectors.joining;
 
 public class Day04 {
 
@@ -20,41 +21,37 @@ public class Day04 {
             var id = parts.get(1).asInt();
             var checksum = parts.get(2).get();
 
-            var counts = new HashMap<Character, Integer>();
-            for (int i = 0; i < name.length(); i++) {
-                char ch = name.charAt(i);
-                if (ch == '-') {
-                    continue;
-                }
-                counts.merge(ch, 1, Integer::sum);
-            }
+            var counts = charStream(name)
+                    .filter(c -> c != '-')
+                    .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 
             var expectedChecksum = counts.entrySet().stream()
-                    .sorted(Comparator.comparing(e -> ((long) -e.getValue()) << 32 | e.getKey()))
+                    .sorted(Entry.<Character, Long>comparingByValue().reversed().thenComparing(Entry::getKey))
                     .limit(5)
                     .map(Entry::getKey)
                     .map(String::valueOf)
-                    .collect(Collectors.joining(""));
+                    .collect(joining(""));
             if (!expectedChecksum.equals(checksum)) {
                 continue;
             }
 
             sum += id;
-            var sb = new StringBuilder();
-            for (int i = 0; i < name.length(); i++) {
-                char ch = name.charAt(i);
-                char realCh = ch == '-'
-                        ? ' '
-                        : (char) ('a' + (((ch - 'a') + id) % 26));
-                sb.append(realCh);
-            }
-            if (sb.toString().equals("northpole object storage")) {
+
+            var realName = charStream(name)
+                    .map(c -> c == '-' ? ' ' : (char) ('a' + (((c - 'a') + id) % 26)))
+                    .map(String::valueOf)
+                    .collect(joining());
+            if (realName.equals("northpole object storage")) {
                 storageId = id;
             }
         }
 
         System.out.println("Part 1: " + sum);
         System.out.println("Part 2: " + storageId);
+    }
+
+    private static Stream<Character> charStream(String str) {
+        return str.chars().mapToObj(i -> (char) i);
     }
 
 }
