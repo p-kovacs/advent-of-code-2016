5ef62bb1552cabb302a7765dcaf50496bb631220
Péter Kovács <kovacspeter84@gmail.com>
2021-11-05 11:37:06 +0100

    Add a simplified solution for Day19

--- src/main/java/pkovacs/aoc/y2016/Day19.java
+++ src/main/java/pkovacs/aoc/y2016/Day19.java
@@ -7,11 +7,12 @@ public class Day19 {
     public static void main(String[] args) {
         int playerCount = Integer.parseInt(InputUtils.readSingleLine("y2016/day19.txt"));
 
-        System.out.println("Part 1: " + play(playerCount, false));
-        System.out.println("Part 2: " + play(playerCount, true));
+        System.out.println("Part 1: " + play2(playerCount, false));
+        System.out.println("Part 2: " + play2(playerCount, true));
     }
 
-    public static int play(int playerCount, boolean advanced) {
+    // Solution with a doubly linked list
+    public static int play1(int playerCount, boolean advanced) {
         // Build a doubly linked cyclic list of the player IDs
         int[] next = new int[playerCount];
         int[] prev = new int[playerCount];
@@ -24,10 +25,28 @@ public class Day19 {
 
         // Play until only one player is left
         int i = advanced ? playerCount / 2 : 1; // denotes the next player to steal from
-        for (int playingCount = playerCount; playingCount > 1; playingCount--) {
+        for (int count = playerCount; count > 1; count--) {
             next[prev[i]] = next[i];
             prev[next[i]] = prev[i];
-            i = advanced && (playingCount % 2 == 0) ? next[i] : next[next[i]];
+            i = advanced && (count % 2 == 0) ? next[i] : next[next[i]];
+        }
+
+        return i + 1;
+    }
+
+    // Simplified solution with a singly linked list
+    public static int play2(int playerCount, boolean advanced) {
+        // Build a cyclic list of the player IDs
+        int[] next = new int[playerCount];
+        for (int i = 0; i < next.length - 1; i++) {
+            next[i] = i + 1;
+        }
+
+        // Play until only one player is left
+        int i = advanced ? playerCount / 2 - 1 : 0; // denotes the player before the next player to steal from
+        for (int count = playerCount; count > 1; count--) {
+            next[i] = next[next[i]];
+            i = advanced && (count % 2 == 0) ? i : next[i];
         }
 
         return i + 1;
